26fed378a8790abbc63f56893bd4bfddcea5c6cf
AnaghaP123 <102337291+AnaghaP123@users.noreply.github.com>
2023-12-11 22:43:19 -0800

    Add files via upload

---
+++ stuff.js
@@ -0,0 +1,16 @@
+page = 1;
+
+function changePage()
+{
+    window.location = "info.html";
+}
+
+function pageChange()
+{
+    window.location = "index.html";
+}
+
+function workPage()
+{
+    window.location = "workscited.html";
+}
